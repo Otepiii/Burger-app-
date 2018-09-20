6c91df864efb35b3fae35dafd20e28691cac1173
Joseph Evangelista <oootep123@gmail.com>
2018-09-19 22:19:07 -0400

    made the submit button work

--- config/orm.js
+++ config/orm.js
@@ -8,16 +8,28 @@ var orm = {
         });
     },
 
-    insertOne: function (burger_name, callback) {
-        console.log("string",burger_name);
+    // insertOne: function (burger_name, callback) {
+    //     console.log("string",burger_name);
 
-        connection.query("INSERT INTO burgers (burger_name, devoured) VALUES (?),(?)", [
-            {burger_name},
-            {devoured:0}
-        ], function (error, res) {
-            if (error) throw error;
-            callback(res);
-        });
+    //     connection.query("INSERT INTO burgers burger_name SET ?", {
+    //         burger_name:burger_name,
+    //         devoured:0
+    // }, function (error, res) {
+    //         if (error) throw error;
+    //         callback(res);
+    //     });
+    // },
+
+    insertOne: function (burger_name, callback) {
+        connection.query('INSERT INTO burgers SET ?',
+            {
+                burger_name: burger_name,
+                devoured: false,
+            }, function (err, result) {
+                if (err) throw err;
+                callback(result);
+            });
+ 
     },
 
     updateOne: function (id, devoured, callback) {
